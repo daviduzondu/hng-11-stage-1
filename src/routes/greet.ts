62320ea2e5becf56c1d939c3b0e8c1d84db9de02
David <uzondudavid2004@gmail.com>
2024-07-01 00:05:20 +0100

    fix issues with ip address geolocation

--- src/routes/greet.ts
+++ src/routes/greet.ts
@@ -5,9 +5,11 @@ const router = Router();
 router.route("/hello").get(async (req: Request, res: Response, next: NextFunction) => {
     const { visitor_name } = req.query;
 
-    const response = await fetch(`https://demo.ip-api.com/json/${req.ip}?fields=66842623&lang=en`);
-    if (!response.ok) return next(new Error("Failed to get ip address"));
+    const response = await fetch(`http://ip-api.com/json/${req.ip}?fields=66842623&lang=en`);
+    
+    if (!response.ok) return next(new Error("failed to get location based on ip address"));
     const { city } = await response.json();
+    if (!city) return next(new Error("failed to get location based on ip address"));
 
     if (!visitor_name) return next(new Error("visitor_name not provided"));
     res.status(200).send({ greeting: `Hello ${visitor_name}`, client_ip: req.ip, location: city });
